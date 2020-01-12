256414b9228f639d642272ec0ebba77598e975f3
dee <deeflash211@gmail.com>
2020-01-12 21:40:37 +0200

    добавил картинок styles/js

--- dekiru/static/js/group.js
+++ dekiru/static/js/group.js
@@ -48,8 +48,8 @@ function createTimeCicles() {
         countDown.each(function (){
             $(this).TimeCircles({
                 'animation'       : 'ticks',
-                'bg_width'        : 0.2,
-                'fg_width'        : 0.01,
+                'bg_width'        : 0.4,
+                'fg_width'        : 0.02,
                 'circle_bg_color' : 'rgba(255,255,255,0.9)',
                 'time'            : {
                     'Days' : {
@@ -79,4 +79,29 @@ function createTimeCicles() {
 }
 
 createTimeCicles();
-// $('div#timer').TimeCircles();
+$(window).resize(function(){
+    $(".count-down").TimeCircles().rebuild();
+});
+
+// from https://signalvnoise.com/posts/2407-device-scale-user-interface-elements-in-ios-mobile-safari
+function getDeviceScale() {
+    var deviceWidth, landscape = Math.abs(window.orientation) == 90;
+    if (landscape) {
+        // iPhone OS < 3.2 reports a screen height of 396px
+        deviceWidth = Math.max(480, screen.height);
+    } else {
+        deviceWidth = screen.width;
+    }
+    return window.innerWidth / deviceWidth;
+}
+
+// mobile only - keep the position:fixed header at constant size when page is zoomed
+if (navigator.userAgent.match(/Mobi/)) {
+    $(window).on('load scroll', function() {
+        var ds = getDeviceScale();
+        $('.device-fixed-height').css('transform','scale(1,' + ds + ')')
+            .css('transform-origin', '0 0');
+        $('.device-fixed-width').css('transform', 'scale(' + ds + ',1)')
+            .css('transform-origin', '0 0');
+    })
+}
